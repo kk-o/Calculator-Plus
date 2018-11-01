a48856cec70dc4f38734ab65b7ac6d531afc248b
Uldarico Estrada <uldarico@outlook.com>
2018-10-31 17:49:29 -0700

    Updates

---
+++ precache-manifest.829369d027201442b633259588ef1f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4203922fb1b3ed02e6b",
+    "url": "/Calculator-Plus/static/js/runtime~main.c4203922.js"
+  },
+  {
+    "revision": "526c38033404a0ba24f4",
+    "url": "/Calculator-Plus/static/js/main.526c3803.chunk.js"
+  },
+  {
+    "revision": "b3291d0755209af01435",
+    "url": "/Calculator-Plus/static/js/1.b3291d07.chunk.js"
+  },
+  {
+    "revision": "526c38033404a0ba24f4",
+    "url": "/Calculator-Plus/static/css/main.2abd3840.chunk.css"
+  },
+  {
+    "revision": "b3291d0755209af01435",
+    "url": "/Calculator-Plus/static/css/1.cfd26ed8.chunk.css"
+  },
+  {
+    "revision": "b80b13618b57434c77606cc43a0f26f2",
+    "url": "/Calculator-Plus/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55e58e51ee17489a61c1af403eb169e3.js"
+  "/Calculator-Plus/precache-manifest.829369d027201442b633259588ef1f18.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Calculator-Plus/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
